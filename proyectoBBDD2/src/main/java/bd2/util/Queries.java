7f7ac8ac2ff024c4adb5a0636979a03f786664b6
SDLaserna <xerox90913@gmail.com>
2015-05-10 09:40:47 -0300

    Query inciso -d (version a consultar)

--- proyectoBBDD2/src/main/java/bd2/util/Queries.java
+++ proyectoBBDD2/src/main/java/bd2/util/Queries.java
@@ -5,17 +5,28 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+
+import bd2.model.PerfilDeAdministrador;
+import bd2.model.PerfilDeUsuario;
 
 public class Queries {
 	
 	private static SessionFactory sessions;
 	
 	public static void main(String[] args) {
-		Configuration cfg = new Configuration();
-		cfg.configure("hibernate/hibernate.cfg.xml");
-		sessions = cfg.buildSessionFactory();
+		
+		Configuration configuration = new Configuration().configure("hibernate/hibernate.cfg.xml");
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().
+		applySettings(configuration.getProperties());
+		sessions= configuration.buildSessionFactory(builder.build());
+//	-----"configuracion deprecated"------
+//		Configuration cfg = new Configuration();
+//		cfg.configure("hibernate/hibernate.cfg.xml");
+//		sessions = cfg.buildSessionFactory();
 		conexion('a');
+		conexion('b');
 	}
 	
 	private static void conexion(char letraConsulta){
@@ -23,13 +34,14 @@ public class Queries {
 		Transaction tx = null;
 		try {
 			tx = session.beginTransaction();
-			System.out.println("----------interlineado--------");
+			System.out.println();
+			System.out.println("--------------------------------------------------------- interlineado --------------------------------------------------");
 			//acá hay que poner un case que dado la letra que entra por parametro es la consulta que llama
 			//si bien se que esta forma no es la mejor en el caso de tener 1000 consultas dado que solo 
 			// tenemos 8 consultas de estar forma reusamos mas codigo y es todo mas facilito
 			switch (letraConsulta){
-			case 'a': listarNombresPizarras(session,tx);
-				break;
+			case 'a': listarNombresPizarras(session,tx); break;
+			case 'b': mailDeAdminsConPizarraArchivada(session,tx); break;
 			}
 			System.out.println("----------interlineado--------");
 			session.flush();
@@ -50,6 +62,17 @@ public class Queries {
 		List<String> nombres= sesion.createQuery(" select p.nombre from Pizarra p").list();
 		for(String unNombre: nombres){
 			System.out.println(unNombre);
+		}
+	}
+	
+	public static void mailDeAdminsConPizarraArchivada(Session sesion, Transaction t){
+		System.out.println("Obtener	los emails de los administradores de los proyectos que tengan al menos una pizarra archivada.");
+		System.out.println("----------interlineado--------");
+		List<String> coleccion= sesion.createQuery("select usuario.email from PerfilDeAdministrador as PDA where PDA.idPerfilUsuario IN " /*Por que tengo que ponerle "PDA.idPerfilUsuario" porque sino no sabe resolver "PDA.idAdministrador"?*/
+																													+ "(select perf.idPerfilUsuario from Proyecto as p inner join p.perfiles as perf where perf.class = PerfilDeAdministrador and p.idProyecto IN "
+																													+ "																																			(select idProyecto from Proyecto where pizarrasArchivadas.size > 0))").list(); /*Aca debe haber un problema, por lo que veo en el DBLoader solo se archivan 2 pizarras, pero al parecer la base de datos me dice que todos los proyectos tienen 3 pizarras archivadas */
+		for(String iteracion: coleccion){
+			System.out.println("Administrador: "+iteracion);
 		}
 	}
 }	
